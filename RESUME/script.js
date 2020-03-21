fa49a89a4b692265747da0191b9278911dba0984
Kevin <goodjman@outlook.com>
2020-03-21 20:30:37 +0300

    Разобрался

--- RESUME/script.js
+++ RESUME/script.js
@@ -1,3 +1,12 @@
 let the ="fun begin";
 let creating='gitignore';
 let thegit="be legit";
+obj={
+    name,
+    mood(){
+        if (name==""){
+            console.log("Nu i mudak ti");
+        }
+    }
+};
+
